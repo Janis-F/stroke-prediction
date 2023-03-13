d35f4bd10f45405289e77081123e6868d7e9d18c
Janis-F <janis.freimanis@hey.com>
2023-03-13 20:13:54 +0200

    Test app load capacity with locust

---
+++ locustfile.py
@@ -0,0 +1,29 @@
+from app.app import Observation
+from locust import HttpUser, between, task
+import random
+
+
+class MyUser(HttpUser):
+    wait_time = between(1, 2)
+    host = "https://default-service-2hwdmv3hoq-nw.a.run.app"
+
+    @task
+    def predict(self):
+        observation = Observation(
+            gender=random.choice(["Male", "Female"]),
+            age=random.uniform(18, 100),
+            hypertension=random.randint(0, 1),
+            heart_disease=random.randint(0, 1),
+            ever_married=random.choice(["Yes", "No"]),
+            work_type=random.choice(
+                ["Private", "Self-employed", "Govt_job", "Never_worked"]),
+            Residence_type=random.choice(["Urban", "Rural"]),
+            avg_glucose_level=random.uniform(50, 300),
+            bmi=random.uniform(15, 50),
+            smoking_status=random.choice(
+                ["formerly smoked", "never smoked", "smokes", "Unknown"]),
+            age_group="",
+            glucose_group=""
+        )
+
+        self.client.post("/predict", json=observation.dict())
